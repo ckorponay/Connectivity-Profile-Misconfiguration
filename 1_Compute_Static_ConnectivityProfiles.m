dcc143442a05bb5e7c87a3899ddcacb7c722be14
ckorponay <64859234+ckorponay@users.noreply.github.com>
2025-08-22 10:49:34 -0400

    Update 1_Compute_Static_ConnectivityProfiles.m

--- 1_Compute_Static_ConnectivityProfiles.m
+++ 1_Compute_Static_ConnectivityProfiles.m
@@ -35,7 +35,9 @@ clearvars; close all; clc
 
 addpath '/path/to/Downloads/NIfTI_20140122'
 
-      
+
+%%RERUN FOR EACH GROUP%% 
+
 
 %% load time series CSV file paths for each subject
 
@@ -58,7 +60,12 @@ Total_Target_ROIs=  ;          %User sets parameter
 
 %% load the time series data for each subject
 
+%Group 1%
 Final_EdgeTimeseries=zeros(Subjects*TRs*Seed_Voxels,Total_Target_ROIs);
+%Final_EdgeTimeseries_Top5=zeros(Subjects*TRs*Seed_Voxels,Restricted_Target_ROIs);
+
+%Group 2%
+%Final_EdgeTimeseries=zeros(Subjects*TRs*Seed_Voxels,Total_Target_ROIs);
 %Final_EdgeTimeseries_Top5=zeros(Subjects*TRs*Seed_Voxels,Restricted_Target_ROIs);
 
 for i = 1:Subjects
@@ -104,7 +111,8 @@ end
 
 %Compute the group-averaged time-averaged connectivity profile of each seed voxel
 
-GroupAvg_ConnProf = mean(AvgConnProf_acrossTime,3);
+GroupAvg_ConnProf_Group1 = mean(AvgConnProf_acrossTime,3);
+%GroupAvg_ConnProf_Group2 = mean(AvgConnProf_acrossTime,3);
 
 
 %%%% Project the connectivity values into an MNI space NIFTI file %%%%
@@ -118,7 +126,8 @@ full_brain_space = double(full_brain_space);
 
 for i=1:Total_Target_ROIs
 
-full_brain_space(in_brain)=GroupAvg_ConnProf(:,i) %%User sets number; grab the seed's voxelwise connectivity value with ROI X
+full_brain_space(in_brain)=GroupAvg_ConnProf_Group1(:,i) %%User sets number; grab the seed's voxelwise connectivity value with ROI X
+%full_brain_space(in_brain)=GroupAvg_ConnProf_Group2(:,i) %%User sets number; grab the seed's voxelwise connectivity value with ROI X
 
 imgFile = sprintf('%s/Seed_Mask.nii.gz', imgRoot);
 gm = load_untouch_nii(imgFile);
@@ -126,7 +135,8 @@ new_hdr=gm;
 new_hdr.vol=full_brain_space;
 new_hdr.img=full_brain_space;
 
-flNm = num2str(i,'Seed_Voxelwise_FC_with_ROI_00%d');
+flNm = num2str(i,'Group1_Seed_Voxelwise_FC_with_ROI_00%d');
+%flNm = num2str(i,'Group2_Seed_Voxelwise_FC_with_ROI_00%d');
 
 save_untouch_nii(new_hdr,flNm);
 
